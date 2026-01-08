e6c16d9a876d127053efae5da8cb62191d075699
Thomas Wodarek <wodarekwebpage@gmail.com>
2026-01-07 19:32:35 -0500

    Revert "test"
    This reverts commit 84929f7130e34fa6db6a59e410683d210ecff1e5.

--- cmd/optimizer_test.go
+++ cmd/optimizer_test.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"context"
 	"os"
 	"path/filepath"
 	"strings"
@@ -25,7 +24,7 @@ func TestOptimizer(t *testing.T) {
 			"--config", testFile,
 		}...))
 		t.Logf("Running pre-optimizer generate: %v", args)
-		assert.Nil(t, rootCmd.ExecuteContext(context.Background()))
+		assert.Nil(t, rootCmd.Execute())
 
 		args = append(args, []string{
 			"optimizer",
@@ -38,7 +37,7 @@ func TestOptimizer(t *testing.T) {
 		// Check if local pref is lowered
 		checkFile, err := os.ReadFile("test-cache/AS65510_EXAMPLE.conf")
 		assert.Nil(t, err)
-		if !strings.Contains(string(checkFile), "bgp_local_pref = 80; # pathvector:localpref") {
+		if !strings.Contains(string(checkFile), "bgp_local_pref = 100; # pathvector:localpref") {
 			t.Errorf("expected bgp_local_pref = 80 but not found in file")
 		}
 	}
