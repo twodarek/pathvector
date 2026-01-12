3fd5f48985e9c3c04f82c9f3351335af2668bd90
Thomas Wodarek <wodarekwebpage@gmail.com>
2026-01-11 23:57:12 -0500

    Add bird2 to arista plugin

--- vendorbuild/arista/build.sh
+++ vendorbuild/arista/build.sh
@@ -8,7 +8,11 @@ for f in pathvector*linux-amd64.rpm; do echo "$f-sha1: $(sha1sum $f | cut -d " "
 #TODO(twodarek): Download bgpq4 and other deps here, add them to manifest.txt, and add them to the below zip command
 
 wget https://kojipkgs.fedoraproject.org//packages/bgpq4/1.15/4.fc43/x86_64/bgpq4-1.15-4.fc43.x86_64.rpm
-echo "primaryRpm: $(ls bgpq4*.rpm)" >> manifest.txt
+ls bgpq4*.rpm >> manifest.txt
 for f in bgpq4*.rpm; do echo "$f-sha1: $(sha1sum $f | cut -d " " -f 1)"; done >> manifest.txt
+
+wget https://kojipkgs.fedoraproject.org//packages/bird2/2.15.1/1.el7/x86_64/bird2-2.15.1-1.el7.x86_64.rpm
+ls bird2*.rpm >> manifest.txt
+for f in bird2*.rpm; do echo "$f-sha1: $(sha1sum $f | cut -d " " -f 1)"; done >> manifest.txt
 
 zip pathvector-"$(git describe --tags "$(git rev-list --tags --max-count=1)" | cut -c2-)"-arista-amd64.swix manifest.txt pathvector*linux-amd64.rpm
